abd52986d3cb61705015a44a9d2bd01ed88e34ad
Passi <chiruclan@t-online.de>
2011-04-10 17:54:11 +0200

    RELEASE 1.4
    Signed-off-by: Passi <chiruclan@t-online.de>

--- redirect.php
+++ redirect.php
@@ -3,7 +3,7 @@ include "lib/config.php";
 include "lib/header.php";
 ?>
 <title>Weiterleiten - <? echo $sitename ?></title>
-<center>Du wirst in 3 Sekunden weitergeleitet</center>
+Du wirst in 3 Sekunden weitergeleitet
 <head><meta http-equiv="refresh" content="3; <? echo $_SERVER['QUERY_STRING'] ?>"></head>
 <?
 echo "<hr>".$footer;
